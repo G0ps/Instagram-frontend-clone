87d8e52ee617709f7393718f9492eb8fc3e172fb
G0ps <driveofgopi@gmail.com>
2025-09-11 11:51:17 +0530

    touchups

--- instagramclonereact/src/SideBar.jsx
+++ instagramclonereact/src/SideBar.jsx
@@ -9,7 +9,7 @@ function SideBar(){
                 <img className="logo-text" src="./src/assets/instagramText.png" alt="TEXT" />
                 <div className="d-flex flex-column">
                     <button className="menu-btn" onClick={() => navigate("/")}>
-                        <i className="bi bi-building"></i> Home
+                        <i className="bi bi-house"></i> Home
                     </button>
                     <button className="menu-btn" onClick={() => navigate("/search")}>
                         <i className="bi bi-search"></i> Search
@@ -18,26 +18,26 @@ function SideBar(){
                         <i className="bi bi-binoculars"></i> Explore
                     </button>
                     <button className="menu-btn" onClick={() => navigate("/reels")}>
-                        <i className="bi bi-camera-reels-fill"></i> Reels
+                        <i className="bi bi-camera-reels"></i> Reels
                     </button>
                     <button className="menu-btn" onClick={() => navigate("/messages")}>
-                        <i className="bi bi-chat-fill"></i> Messages
+                        <i className="bi bi-chat"></i> Messages
                     </button>
                     <button className="menu-btn" onClick={() => navigate("/notifications")}>
-                        <i className="bi bi-bell-fill"></i> Notifications
+                        <i className="bi bi-bell"></i> Notifications
                     </button>
                     <button className="menu-btn" onClick={() => navigate("/create")}>
-                        <i className="bi bi-building"></i> Create
+                        <i className="bi bi-file-plus"></i> Create
                     </button>
                     <div className="menu-btn" onClick={() => navigate("/profile")}>
-                        <i className="bi bi-building"></i> Profile
+                        <i className="bi bi-person"></i> Profile
                     </div>
                     </div>
 
             </div>
-            <div className="position-fixed bottom-0 d-flex flex-column gap-3 mb-3">
-                <div><i className="bi bi-building"></i>Threads</div>
-                <div><i className="bi bi-building"></i>More</div>
+            <div className="position-fixed bottom-0 d-flex flex-column mb-3">
+                <div className="menu-btn"><i className="bi bi-threads"></i>Threads</div>
+                <div className="menu-btn"><i className="bi bi-three-dots"></i>More</div>
             </div>
         </>
     )
